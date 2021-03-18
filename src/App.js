8c8603bac316681a6f983600f6152a56609a0047
Johannes de Vries <andriesjohannesdv@gmail.com>
2021-03-18 23:21:05 +0200

    Added Leaflet Map
    I used the Leaflet Map library to display the world map with basic functionality and added basic CSS layout for app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import './styles/app.css';
+import MapLeaflet from './components/MapLeaflet';
 
 function App() {
-  return <div>test</div>;
+  return (
+    <div className={'app-container'}>
+      <div className={'sidebar'}>
+        <h1>Info</h1>
+      </div>
+      <div className={'map'}>
+        <MapLeaflet />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/MapLeaflet.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/mapLeaflet.css';
+import { Map, TileLayer } from 'react-leaflet';
+
+const MapLeaflet = () => {
+  return (
+    <Map center={[-30.559483, 22.937506]} zoom={6}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </Map>
+  );
+};
+
+export default MapLeaflet;
