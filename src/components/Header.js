aac3dd0a46ac757a1929c9d73442a4e7edf2ffd2
Johannes de Vries <andriesjohannesdv@gmail.com>
2021-03-19 02:37:12 +0200

    Added Add Location button in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,14 @@ import '../styles/header.css';
 const Header = () => {
   return (
     <div className="header-container">
-      <div className="test">
+      <div className="logo-name-container">
         <h1 className="logo">
           <FaMapMarkerAlt />
         </h1>
         <h1 className="name">AnyWeather</h1>
       </div>
       <div>
-        <button>test</button>
+        <button className="button-add-location">Add Location</button>
       </div>
     </div>
   );
